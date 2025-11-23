66d15ff02f9fcd95c6e552dbd2540427ef1c3c45
Andrey <cshmuch@mail.ru>
2025-11-24 01:23:16 +0300

    backend & static & gateway

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -96,8 +96,8 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static') 
+STATIC_URL = '/backend/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static/backend') 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
