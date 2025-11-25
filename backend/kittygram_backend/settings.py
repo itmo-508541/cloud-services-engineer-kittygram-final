93267cc011680998d68193824453f0b641198ec9
Andrey <cshmuch@mail.ru>
2025-11-25 21:45:43 +0300

    DEBUG=1 is debug mode

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = not not os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG') == "1"
 
 ALLOWED_HOSTS = ['*']
 
